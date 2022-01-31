e36704e192ec84e809e394103b00d5db6f6a1d17
NaiduMidatana <96723183+NaiduMidatana@users.noreply.github.com>
2022-01-31 09:13:12 +0530

    bms project

---
+++ BankManagementSystem/src/bankmanagementsystem/model/Bank.java
@@ -0,0 +1,10 @@
+package bankmanagementsystem.model;
+
+public class Bank {
+
+	public int bankId = 654645 ;
+	public String bankName = "MOURITECH BANK";
+	public String bankLocation = "VIZAG";
+	
+	
+}
---
+++ BankManagementSystem/src/bankmanagementsystem/utils/DBConnection.java
@@ -0,0 +1,26 @@
+package bankmanagementsystem.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class DBConnection {
+	static Connection con = null;
+	static Statement stmt = null;
+
+	public static Connection getDBConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// step 2 => Establish the connection to DB
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/naidudb", "root", "root");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
+		return con;
+	}
+}
+
